a7fe974bc10180c89309e7af3d2707743a65f1e0
Luis Canchola <luiscanchola49@aragon.unam.mx>
2026-07-25 01:45:48 -0600

    fix: agregar comentario para desactivar la regla de exhaustividad en useEffect

--- app/dashboard/insurers/page.tsx
+++ app/dashboard/insurers/page.tsx
@@ -37,6 +37,7 @@ export default function InsurersPage() {
 
   useEffect(() => {
     loadData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   async function loadData() {
